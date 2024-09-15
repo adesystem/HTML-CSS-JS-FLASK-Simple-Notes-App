6c4a97730e6aee5d813463da2f4d5f4c4d3bf3b4
alewand2 <01187246@pw.edu.pl>
2024-09-15 22:06:32 +0200

    Created database integration.

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_NAME: str = os.getenv("DATABASE_NAME")
+DATABASE_USER: str = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD: str = os.getenv("DATABASE_PASSWORD")
+DATABASE_HOST: str = os.getenv("DATABASE_HOST", "localhost")
+DATABASE_PORT: str = os.getenv("DATABASE_PORT", "3306")
+
+SECRET_KEY: str = os.getenv("SECRET_KEY")
+DEBUG_MODE: bool = os.getenv("DEBUG_MODE", False)
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'very_secret_key'
+    app.config['SECRET_KEY'] = SECRET_KEY
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
+    db.init_app(app)
 
     from website.routes.views import views
     from website.routes.auth import auth
